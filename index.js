b732ad9d7e81b8dd9174e21cca2caddf70059f85
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-09 06:24:00 +0700

    Menambahkan gravitasi dan permulaan untuk movement

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ console.log(c);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 0.2;
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
     };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
     this.width = 30;
     this.height = 30;
   }
@@ -20,7 +26,48 @@ class Player {
     c.fillStyle = "teal";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else this.velocity.y = 0;
+  }
 }
 
 const player = new Player();
-player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  // console.log('go')
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
+
+window.addEventListener("keydown", ({ keyCode }) => {
+  // console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      break;
+
+    case 68:
+      console.log("right");
+      break;
+
+    case 83:
+      console.log("down");
+      break;
+
+    case 87:
+      console.log("up");
+      break;
+
+    default:
+      break;
+  }
+});
