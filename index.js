8eab3c86675f7799d52505e5f68b42b35b9a4967
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-09 09:17:08 +0700

    Membuat platform beserta collision-nya, dan scrolling layar

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 console.log(c);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const gravity = 0.2;
 class Player {
@@ -38,7 +38,26 @@ class Player {
   }
 }
 
+class Platform {
+  constructor({ x, y }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+    this.height = 20;
+  }
+
+  draw() {
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+
+const platforms = [new Platform({ x: 200, y: 100 }), new Platform({ x: 500, y: 200 })];
+
 const keys = {
   right: {
     pressed: false,
@@ -48,22 +67,53 @@ const keys = {
   },
 };
 
+let scrollOffset = 0;
+
 function animate() {
   requestAnimationFrame(animate);
   // console.log('go')
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
 
-  if (keys.right.pressed) {
+  if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
-  } else if (keys.left.pressed) {
+  } else if (keys.left.pressed && player.position.x > 100) {
     player.velocity.x = -5;
-  } else player.velocity.x = 0;
+  } else {
+    player.velocity.x = 0;
+    // screen scrolling
+    if (keys.right.pressed) {
+      scrollOffset += 5;
+      platforms.forEach((platform) => {
+        platform.position.x -= 5;
+      });
+    } else if (keys.left.pressed) {
+      scrollOffset -= 5;
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
+    }
+  }
+
+  // Platform collision detection
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >= platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
+    }
+  });
 }
 
 animate();
 
-window.addEventListener("keydown", ({ keyCode }) => {
+addEventListener("keydown", ({ keyCode }) => {
   // console.log(keyCode);
   switch (keyCode) {
     case 65:
@@ -90,7 +140,7 @@ window.addEventListener("keydown", ({ keyCode }) => {
   }
 });
 
-window.addEventListener("keyup", ({ keyCode }) => {
+addEventListener("keyup", ({ keyCode }) => {
   // console.log(keyCode);
   switch (keyCode) {
     case 65:
@@ -109,7 +159,7 @@ window.addEventListener("keyup", ({ keyCode }) => {
 
     case 87:
       console.log("up");
-      // player.velocity.y -= 10;
+      player.velocity.y += 1;
       break;
 
     default:
