0e3eb26b34818f5084596508da0cd33470dfaa9b
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-09 07:22:41 +0700

    Menambahkan movement

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
@@ -38,12 +39,26 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   // console.log('go')
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else player.velocity.x = 0;
 }
 
 animate();
@@ -53,10 +68,12 @@ window.addEventListener("keydown", ({ keyCode }) => {
   switch (keyCode) {
     case 65:
       console.log("left");
+      keys.left.pressed = true;
       break;
 
     case 68:
       console.log("right");
+      keys.right.pressed = true;
       break;
 
     case 83:
@@ -65,6 +82,34 @@ window.addEventListener("keydown", ({ keyCode }) => {
 
     case 87:
       console.log("up");
+      player.velocity.y -= 10;
+      break;
+
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ keyCode }) => {
+  // console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+
+    case 83:
+      console.log("down");
+      break;
+
+    case 87:
+      console.log("up");
+      // player.velocity.y -= 10;
       break;
 
     default:
